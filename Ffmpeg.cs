77aba9a275ad2531987038fbecc4abba999c9146
qwer327146 <qwer327146@gmail.com>
2020-02-11 10:14:44 +0800

    初始化UI

--- Ffmpeg.cs
+++ Ffmpeg.cs
@@ -8,5 +8,16 @@ namespace Ffmpeg_With_Shell
 {
     class Ffmpeg
     {
+        private string ffmpegPath;
+        private string ffplayPath;
+        private string ffprobePath;
+
+        private string[] arguments;
+                
+
+        public string FfmpegPath { get => ffmpegPath; set => ffmpegPath = value; }
+        public string FfplayPath { get => ffplayPath; set => ffplayPath = value; }
+        public string FfprobePath { get => ffprobePath; set => ffprobePath = value; }
+        public string[] Arguments { get => arguments; set => arguments = value; }
     }
 }
--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Ffmpeg_With_Shell
 {
-    partial class Form1
+    partial class Form_Main
     {
         /// <summary>
         /// 必需的设计器变量。
@@ -28,13 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.splitContainer_Main = new System.Windows.Forms.SplitContainer();
+            this.tabControl_Main = new System.Windows.Forms.TabControl();
+            this.tabPage_AssConverter = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textBox_Output = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer_Main)).BeginInit();
+            this.splitContainer_Main.Panel1.SuspendLayout();
+            this.splitContainer_Main.Panel2.SuspendLayout();
+            this.splitContainer_Main.SuspendLayout();
+            this.tabControl_Main.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer_Main
+            // 
+            this.splitContainer_Main.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer_Main.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer_Main.Name = "splitContainer_Main";
+            // 
+            // splitContainer_Main.Panel1
+            // 
+            this.splitContainer_Main.Panel1.Controls.Add(this.tabControl_Main);
+            // 
+            // splitContainer_Main.Panel2
+            // 
+            this.splitContainer_Main.Panel2.Controls.Add(this.textBox_Output);
+            this.splitContainer_Main.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer_Main.SplitterDistance = 191;
+            this.splitContainer_Main.TabIndex = 0;
+            // 
+            // tabControl_Main
+            // 
+            this.tabControl_Main.Controls.Add(this.tabPage_AssConverter);
+            this.tabControl_Main.Controls.Add(this.tabPage2);
+            this.tabControl_Main.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl_Main.Location = new System.Drawing.Point(0, 0);
+            this.tabControl_Main.Name = "tabControl_Main";
+            this.tabControl_Main.SelectedIndex = 0;
+            this.tabControl_Main.Size = new System.Drawing.Size(191, 450);
+            this.tabControl_Main.TabIndex = 0;
+            // 
+            // tabPage_AssConverter
+            // 
+            this.tabPage_AssConverter.Location = new System.Drawing.Point(4, 22);
+            this.tabPage_AssConverter.Name = "tabPage_AssConverter";
+            this.tabPage_AssConverter.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage_AssConverter.Size = new System.Drawing.Size(183, 424);
+            this.tabPage_AssConverter.TabIndex = 0;
+            this.tabPage_AssConverter.Text = "Ass字幕转换";
+            this.tabPage_AssConverter.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(192, 74);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // textBox_Output
+            // 
+            this.textBox_Output.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox_Output.Location = new System.Drawing.Point(0, 0);
+            this.textBox_Output.Multiline = true;
+            this.textBox_Output.Name = "textBox_Output";
+            this.textBox_Output.Size = new System.Drawing.Size(605, 450);
+            this.textBox_Output.TabIndex = 0;
+            // 
+            // Form_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.splitContainer_Main);
+            this.Name = "Form_Main";
+            this.Text = "ffmpeg助手";
+            this.splitContainer_Main.Panel1.ResumeLayout(false);
+            this.splitContainer_Main.Panel2.ResumeLayout(false);
+            this.splitContainer_Main.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer_Main)).EndInit();
+            this.splitContainer_Main.ResumeLayout(false);
+            this.tabControl_Main.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer_Main;
+        private System.Windows.Forms.TabControl tabControl_Main;
+        private System.Windows.Forms.TabPage tabPage_AssConverter;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox textBox_Output;
     }
 }
 
