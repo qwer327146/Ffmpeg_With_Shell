d2fc0e91fd28e5f6c167687fe0f52264d73b06a6
大fa师 <chfreezer00@gmail.com>
2020-02-12 03:12:29 +0800

    1. 新增帧率和分辨率的预设 2. 自动识别ass文件结束时间 3. process线程改async函数

---
+++ FrameRate.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ffmpeg_With_Shell
+{
+    public partial class FrameRate : Form
+    {
+        public FrameRate()
+        {
+            InitializeComponent();
+            textBox_FrameRate.Text = Main.CustomFrameRate.ToString();
+        }
+
+        private void button_Submit_Click(object sender, EventArgs e)
+        {
+            float.TryParse(textBox_FrameRate.Text, out float frameRate);
+            if (frameRate > 0)
+            {
+                Main.CustomFrameRate = frameRate;
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("请填入有效数字！");
+            }
+        }
+
+        private void textBox_FrameRate_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
